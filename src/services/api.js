2c5e6780e42532fabc491a5b73da4c1d1c715d35
hajarsalhi <90557133+hajarsalhi@users.noreply.github.com>
2025-01-16 15:00:50 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://book-store-server-z514.onrender.com';
 
 // Create axios instance
 const api = axios.create({
