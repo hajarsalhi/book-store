ca3c3bad01341832d7f10c078e4a732800c0ac58
hajarsalhi <shajar32@gmail.com>
2025-01-21 12:34:54 +0100

    fix import error

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,8 +1,7 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../../context/AuthContext'; // Assuming you have an AuthContext
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useAuth();
+  const { user } = localStorage.getItem('user');
   const location = useLocation();
 
   if (!user) {
